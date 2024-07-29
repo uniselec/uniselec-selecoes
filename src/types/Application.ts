c0ff3e691e54360107e9ea0f6e55ad2644478c8e
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-07-29 11:12:34 -0300

    Type Application

--- src/types/Application.ts
+++ src/types/Application.ts
@@ -1,20 +1,37 @@
-
-
 export interface Results {
-    data:  Application[];
-    links: Links;
-    meta:  Meta;
+  data: Application[];
+  links: Links;
+  meta: Meta;
 }
+
 export interface Result {
-    data:  Application;
+  data: Application;
 }
 
 export interface Application {
-    id:              number;
-    data:            string;
-    created_at: null | string;
-    updated_at: null | string;
+  id: number;
+  data: {
+    name: string;
+    cpf: string;
+    enem: string;
+    dob: string;
+    sex: string;
+    email: string;
+    phone1: string;
+    phone2?: string;
+    address: string;
+    uf: string;
+    city: string;
+    campus: string;
+    course: string;
+    vaga: string[];
+    publicSchool: boolean;
+    termsAgreement: boolean;
+  };
+  created_at: null | string;
+  updated_at: null | string;
 }
+
 export interface Links {
   prev: string;
   last: string;
