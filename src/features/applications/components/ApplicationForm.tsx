ae701ae26e698c3e5b324b3da6772fbe44c37eb2
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-07-29 22:58:59 -0300

    Fix form.

--- src/features/applications/components/ApplicationForm.tsx
+++ src/features/applications/components/ApplicationForm.tsx
@@ -50,25 +50,6 @@ const bonusOptions = [
   { label: "Nenhuma das anteriores", value: "none" },
 ];
 
-const isValidCPF = (cpf: string) => {
-  cpf = cpf.replace(/[^\d]+/g, ""); // Remove caracteres não numéricos
-  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
-
-  let sum = 0;
-  let remainder;
-  for (let i = 1; i <= 9; i++) sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
-  remainder = (sum * 10) % 11;
-
-  if (remainder === 10 || remainder === 11) remainder = 0;
-  if (remainder !== parseInt(cpf.substring(9, 10))) return false;
-
-  sum = 0;
-  for (let i = 1; i <= 10; i++) sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
-  remainder = (sum * 10) % 11;
-
-  if (remainder === 10 || remainder === 11) remainder = 0;
-  return remainder === parseInt(cpf.substring(10, 11));
-};
 
 export function ApplicationForm({
   application,
@@ -87,6 +68,7 @@ export function ApplicationForm({
         ...prevState,
         name: userAuth.name,
         email: userAuth.email,
+        cpf: userAuth.cpf,
       }));
     }
   }, [userAuth]);
@@ -107,15 +89,7 @@ export function ApplicationForm({
     setFormState({ ...formState, bonus: e.target.value === "none" ? [] : [e.target.value] });
   };
 
-  const handleCpfChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setFormState({ ...formState, cpf: value });
-    if (value && !isValidCPF(value)) {
-      setCpfError("CPF inválido");
-    } else {
-      setCpfError(null);
-    }
-  };
+
 
   return (
     <Box p={2}>
@@ -170,10 +144,10 @@ export function ApplicationForm({
                 name="cpf"
                 label="CPF do Candidato"
                 value={formState.cpf || ""}
-                onChange={handleCpfChange}
+                onChange={(e) => setFormState({ ...formState, cpf: e.target.value })}
                 error={!!cpfError}
                 helperText={cpfError || ""}
-                disabled={isdisabled}
+                disabled
                 data-testid="cpf"
               />
             </FormControl>
