810f8bf7b038877d88286758a6eeeed607433f72
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-08-01 11:27:01 -0300

    Fix dates

--- src/features/applications/components/ApplicationTable.tsx
+++ src/features/applications/components/ApplicationTable.tsx
@@ -6,6 +6,9 @@ import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import LogoUNILAB from "../../../assets/img/logo-unilab-preto.png";
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
 
 const ImageLogo = styled('img')`
   width: 300px;
@@ -58,11 +61,14 @@ export function ApplicationTable({ applications, isFetching }: Props) {
                   <Typography variant="subtitle1" align="center">
                     PROCESSO SELETIVO UNILAB – (MODELO SISU) - INGRESSO NO PERÍODO LETIVO 2024.1
                   </Typography><br/>
+                  <Typography variant="body1">
+                    <strong>Data de Nascimento:</strong> {}
+                  </Typography>
                   <Typography variant="body1"><strong>Nome Completo:</strong> {value.data.name}</Typography>
                   <Typography variant="body1"><strong>Nome Social:</strong> {value.data.social_name || "Não informado"}</Typography>
                   <Typography variant="body1"><strong>Email:</strong> {value.data.email}</Typography>
                   <Typography variant="body1"><strong>CPF:</strong> {value.data.cpf}</Typography>
-                  <Typography variant="body1"><strong>Data de Nascimento:</strong> {value.data?.birtdate}</Typography>
+                  <Typography variant="body1"><strong>Data de Nascimento:</strong>  {format(parseISO(value.data.birtdate), 'dd/MM/yyyy', { locale: ptBR })}</Typography>
                   <Typography variant="body1"><strong>Sexo:</strong> {value.data.sex}</Typography>
                   <Typography variant="body1"><strong>Telefone 1:</strong> {value.data.phone1}</Typography>
                   <Typography variant="body1"><strong>Endereço:</strong> {value.data.address}</Typography>
